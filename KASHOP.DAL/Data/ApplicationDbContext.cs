4930e527124a553f7beb8da61031e533ba21a754
SajaAsfour <sajanazih2004@gmail.com>
2026-03-05 13:26:10 +0200

    Idintity

--- KASHOP.DAL/Data/ApplicationDbContext.cs
+++ KASHOP.DAL/Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using KASHOP.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KASHOP.DAL.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet <Category> Categories { get; set; }
         public DbSet <CategoryTranslation> CategoriesTranslation { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+        }
     }
 }
