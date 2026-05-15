778415f4b90d3ca0022fff03ac52afa2f50d3ee7
SajaAsfour <sajanazih2004@gmail.com>
2026-05-15 18:44:28 +0300

    user management intrface

---
+++ KASHOP.BLL/Service/IUserManagementSerivce.cs
@@ -0,0 +1,18 @@
+﻿using KASHOP.DAL.DTO.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.BLL.Service
+{
+    public interface IUserManagementSerivce
+    {
+        Task<List<UserListResponse>> GetAllUsersAsync();
+        Task<UserDetailsResponse> GetUserAsync(string userId);
+        Task<bool> ChangeRoleAsync(string userId, string role);
+        Task<bool> ToggleBlockUserAsync(string userId);
+        Task<bool> DeleteUserAsync(string userId);
+    }
+}
---
+++ KASHOP.BLL/Service/UserManagementSerivce.cs
@@ -0,0 +1,46 @@
+﻿using KASHOP.DAL.DTO.Response;
+using KASHOP.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.BLL.Service
+{
+    public class UserManagementSerivce : IUserManagementSerivce
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserManagementSerivce(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<bool> ChangeRoleAsync(string userId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserListResponse>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDetailsResponse> GetUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ToggleBlockUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
